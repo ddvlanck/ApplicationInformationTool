b4e7e518dde2bb4436aac080cebc7f2de592f044
astverha <arne.verhaeghe@ugent.be>
2018-09-12 14:31:42 +0200

    removing old jar, copy new version, update version file

--- SoftwareUpdateTool/src/softwareupdatetool/VersionUpdater.java
+++ SoftwareUpdateTool/src/softwareupdatetool/VersionUpdater.java
@@ -5,25 +5,20 @@
  */
 package softwareupdatetool;
 
-import static com.sun.org.apache.xerces.internal.xinclude.XIncludeHandler.BUFFER_SIZE;
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.json.simple.JSONObject;
@@ -50,6 +45,19 @@ public final class VersionUpdater {
             this.version = br.readLine();
         } catch (IOException e) {
             Logger.getLogger(VersionUpdater.class.getName()).log(Level.SEVERE, "[VersionUpdate]: problems with reading file.");
+        }
+    }
+
+    public void setSoftwareVersion(String version) {
+        File fnew = new File(this.getClass().getResource("/version").getPath());
+        FileWriter f2;
+
+        try {
+            f2 = new FileWriter(fnew, false);
+            f2.write(version);
+            f2.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
@@ -81,12 +89,30 @@ public final class VersionUpdater {
         //  DEVELOPING PHASE
         //  There are 2 jar files, and we copy the newest the the folder of the other one and remove the old version
         //  In this case, the downloadURL is the name of the newest jar file
-        File file = new File("ApplicationInformationTool_v2");
-        System.out.println(file.getAbsolutePath());
         try {
-            Runtime.getRuntime().exec("cp " + file.getAbsolutePath() + " C:\\Users\\dwigh\\Documents\\NetBeansProjects\\AIT\\ApplicationInformationTool\\SoftwareUpdateTool\\src\\currentVersion");
-        } catch(Exception e){
-            e.printStackTrace();
+            URL url = this.getClass().getResource("/newVersion/ApplicationInformationTool_v2.jar");
+            URL dir = this.getClass().getResource("/currentVersion");
+            System.out.println("Url: " + url.getPath() + " \ndir: " + dir.getPath());
+
+            //remove old version
+            URL old = this.getClass().getResource("/currentVersion/ApplicationInformationTool.jar");
+            if (old.getPath() != null) {
+                System.out.println("old: " + old.getPath());
+                Files.deleteIfExists(Paths.get(old.getPath()));
+            }
+
+            //copy new version to currentVersion directory
+            Path temp = Files.copy(Paths.get(url.getPath()), Paths.get(dir.getPath() + "/ApplicationInformationTool.jar"));
+
+            if (temp != null) {
+                System.out.println("File moved successfully");
+                this.setSoftwareVersion("2.0");
+            } else {
+                System.out.println("Failed to move the file");
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(VersionUpdater.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
